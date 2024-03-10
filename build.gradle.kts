d9917cac1b6f1e35429e8c7400c65c1c2b346c48
shalenMathew <shalenmj@gmail.com>
2024-03-10 10:45:30 +0530

    made some changes in main branch

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,7 +2,7 @@
 
 
 plugins {
-    id("com.android.application") version "8.2.0" apply false
+    id("com.android.application") version "8.3.0" apply false
     id("org.jetbrains.kotlin.android") version "1.9.0" apply false
     id("com.google.devtools.ksp") version "1.9.10-1.0.13" apply false
 
